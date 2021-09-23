7bcb1a732e86d23801a39dfcc646c43898d7dc0f
danyamamin99 <danya.mamin.99@bk.ru>
2021-09-23 03:16:11 +0300

    1 day - accordion

---
+++ accordion.js
@@ -0,0 +1,19 @@
+const btns = document.querySelectorAll('.feature__link');
+const lists = document.querySelectorAll('.feature-sub');
+
+btns.forEach( (btn, index) => {
+  btn.addEventListener('click', () => {
+    
+    if (btn.classList.contains('feature__link_active')) {
+      btn.classList.remove('feature__link_active');
+      lists[index].classList.add('hidden');
+    } else {
+      btns.forEach(btn => btn.classList.remove('feature__link_active'));
+      lists.forEach(list => list.classList.add('hidden'));
+
+      btn.classList.add('feature__link_active');
+      lists[index].classList.remove('hidden');
+    }
+
+  });
+});
